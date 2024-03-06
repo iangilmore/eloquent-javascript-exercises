63b14cfc42b6d1e72fda1aef5806a219decd8242
Ian Gilmore <ian.gilmore@me.com>
2024-03-06 17:55:49 -0500

    FizzBuzz

--- exercises.js
+++ exercises.js
@@ -8,3 +8,18 @@ function makeATriangle() {
     console.log("#".repeat(index));
   }
 }
+
+// FizzBuzz
+function fizzerBuzzer() {
+  for (let index = 1; index <= 100; index++) {
+    if (index % 3 === 0 && index % 5 === 0) {
+      console.log("FizzBuzz")
+    } else if (index % 3 === 0) {
+      console.log("Fizz")
+    } else if (index % 5 === 0) {
+      console.log("Buzz")
+    } else {
+      console.log(index)
+    }
+  }
+}
