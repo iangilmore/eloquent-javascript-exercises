cb4052a0937bf05cb6cac73e5e7990655eb1fbc5
Ian Gilmore <ian.gilmore@me.com>
2024-03-06 17:51:10 -0500

    looping a triangle

---
+++ exercises.js
@@ -0,0 +1,10 @@
+// Eloquent Javascript Exercises
+
+// CHAPTER 2
+
+// Looping a triangle
+function makeATriangle() {
+  for (let index = 0; index <= 7; index++) {
+    console.log("#".repeat(index));
+  }
+}
